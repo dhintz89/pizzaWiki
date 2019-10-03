4c5e92965215c1facbee9332775163a692262a46
Quantropolis <u18at19@abdn.ac.uk>
2019-10-03 10:47:38 +0100

    Update required gems
    added active record gem requirement

--- index.rb
+++ index.rb
@@ -1,32 +1,33 @@
 require 'sinatra' # includes gem to process web pages
+require 'sinatra/activerecord'
 set :bind, '0.0.0.0' # sets default url address to localhost
 
-$myinfo = "Pizza Team" 
-@info = "" 
+$myinfo = "Pizza Team"
+@info = ""
 
 
 def readFile(filename)# crates a method to read a file and puts its info into a global varible
-info = "" 
-file = File.open(filename) 
-file.each do |line| 
-	info = info + line 
-end 
+info = ""
+file = File.open(filename)
+file.each do |line|
+	info = info + line
+end
 file.close
-$myinfo = info 
-end 
+$myinfo = info
+end
 
-get '/' do  #binds the route url to the ruby route 
-info = "Welcome!"  
+get '/' do  #binds the route url to the ruby route
+info = "Welcome!"
 len = info.length #NOTE: COME BACK AND FIGURE OUT HOW THIS SHIT WORKS AND COMMENT ABOU IT
-len1 = len 
-readFile("wiki.txt") #invokes a method to read the info from a text file 
+len1 = len
+readFile("wiki.txt") #invokes a method to read the info from a text file
 @info = info + " " + $myinfo #puts the text file info into an object
-len = @info.length 
-len2 = len - 1 
-len3 = len2 - len1 
+len = @info.length
+len2 = len - 1
+len3 = len2 - len1
 @words = len3.to_s #counts the number of words and puts it in an object
 erb :home #executetes the external view for the homepage
-end 
+end
 
 get '/about' do #executetes the external view for the about page
 erb :about
@@ -55,10 +56,10 @@ put '/edit' do #sub program to write user created wiki info to file
 	file.close
 	redirect '/'#the redirect to the hompage signials to the user that their queery has been recived.
 	end
-	
-	
+
+
 # get '/noaccount' do
-    # erb :noaccount 
+    # erb :noaccount
 # end
 
 # get '/denied' do
@@ -66,32 +67,32 @@ put '/edit' do #sub program to write user created wiki info to file
 # end
 
 # get '/admincontrols' do
-     # # protected! 
+     # # protected!
     # @list2 = User.all.sort_by { |u| [u.id] }
     # erb :admincontrols
 # end
 
 
 # get '/notfound' do
-    # erb :notfound 
+    # erb :notfound
 # end
 
 
 # put '/user/:uzer' do
-    # n = User.where(:username => params[:uzer]).to_a.first 
-    # n.edit = params[:edit] ? 1 : 0 
-    # n.save 
-    # redirect '/' 
+    # n = User.where(:username => params[:uzer]).to_a.first
+    # n.edit = params[:edit] ? 1 : 0
+    # n.save
+    # redirect '/'
 # end
 
-# get '/user/delete/:uzer' do   
+# get '/user/delete/:uzer' do
     # # protected!
     # n = User.where(:username => params[:uzer]).to_a.first
-    # if n.username == "Admin" 
-	  	# erb :denied 
-    # else 
-	  	# n.destroy    
-	  	# @list2 = User.all.sort_by { |u| [u.id] } 
+    # if n.username == "Admin"
+	  	# erb :denied
+    # else
+	  	# n.destroy
+	  	# @list2 = User.all.sort_by { |u| [u.id] }
         		# erb :admincontrols
     # end
 # end
