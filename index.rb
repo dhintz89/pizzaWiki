f44812312e9b40a8343d251089a88d5f2615bf2e
Quantropolis <u18at19@abdn.ac.uk>
2019-10-07 00:52:46 +0100

    Basic login system
    Added the backbone of the database and login system; made slight edits to layout and main page to facilitate use and testing of the login system, which appears to be in a working state

--- index.rb
+++ index.rb
@@ -2,50 +2,60 @@ require 'sinatra' # includes gem to process web pages
 require 'sinatra/activerecord'
 set :bind, '0.0.0.0' # sets default url address to localhost
 
+ActiveRecord::Base.establish_connection(
+  :adapter => 'sqlite3',
+  :database => 'pizzaWiki.db'
+)
+
+class User < ActiveRecord::Base
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true
+end
+
 $myinfo = "Pizza Team"
 @info = ""
 
 
 def readFile(filename)# crates a method to read a file and puts its info into a global varible
-info = ""
-file = File.open(filename)
-file.each do |line|
-	info = info + line
-end
-file.close
-$myinfo = info
+	info = ""
+	file = File.open(filename)
+	file.each do |line|
+		info = info + line
+	end
+	file.close
+	$myinfo = info
 end
 
 get '/' do  #binds the route url to the ruby route
-info = "Welcome!"
-len = info.length #NOTE: COME BACK AND FIGURE OUT HOW THIS SHIT WORKS AND COMMENT ABOU IT
-len1 = len
-readFile("wiki.txt") #invokes a method to read the info from a text file
-@info = info + " " + $myinfo #puts the text file info into an object
-len = @info.length
-len2 = len - 1
-len3 = len2 - len1
-@words = len3.to_s #counts the number of words and puts it in an object
-erb :home #executetes the external view for the homepage
+	info = "Welcome!"
+	len = info.length #creates variable len and assigns it the integer value of the number of characters in the info string
+	len1 = len
+	#readFile("wiki.txt") #invokes a method to read the info from a text file (Commented whole line out until wiki.txt exists)
+	@info = info + " " + $myinfo #puts the text file info into an object
+	len = @info.length
+	len2 = len - 1
+	len3 = len2 - len1
+	@words = len3.to_s #counts the number of words and puts it in an object
+	erb :home #executetes the external view for the homepage
 end
 
 get '/about' do #executetes the external view for the about page
-erb :about
+	erb :about
 end
 
 get '/create' do #executetes the external view for the create page
-erb :create
+	erb :create
 end
 
 get'/edit' do #executetes the external view for the homepage
-info = ""
-file = File.open("wiki.txt")
-file.each do |line|
-	info = info + line #extracts all the wiki texs and puts it into a varible
-end
-file.close
-@info = info #puts the varible into an object so the user can see what he is editing
-erb:edit
+	info = ""
+	file = File.open("wiki.txt")
+	file.each do |line|
+		info = info + line #extracts all the wiki texs and puts it into a varible
+	end
+	file.close
+	@info = info #puts the varible into an object so the user can see what he is editing
+	erb:edit
 end
 
 put '/edit' do #sub program to write user created wiki info to file
@@ -55,7 +65,7 @@ put '/edit' do #sub program to write user created wiki info to file
 	file.puts @info #commits the info to the file
 	file.close
 	redirect '/'#the redirect to the hompage signials to the user that their queery has been recived.
-	end
+end
 
 
 # get '/noaccount' do
@@ -72,20 +82,62 @@ put '/edit' do #sub program to write user created wiki info to file
     # erb :admincontrols
 # end
 
+get '/login' do
+    erb :login
+end
+
+post '/login' do
+    $credentials = [params[:username],params[:password]]
+    @Users = User.where(:username => $credentials[0]).to_a.first
+    if @Users
+        if @Users.password == $credentials[1]
+            redirect '/'
+        else
+            $credentials = ['','']
+            redirect '/wrongaccount'
+        end
+    else
+        $credentials = ['','']
+        redirect '/wrongaccount'
+    end
+end
+
+get '/wrongaccount' do
+    erb :wrongaccount
+end
+
+get '/createaccount' do
+    erb :createaccount
+end
+
+post '/createaccount' do  n = User.new
+     n.username = params[:username]
+     n.password = params[:password]
+     if n.username == "Admin" and n.password == "Password"
+	  	n.edit = true
+	 end
+     n.save
+    redirect "/"
+end
+
+get '/logout' do
+    $credentials = ['','']
+    redirect '/'
+end
+
 
 # get '/notfound' do
     # erb :notfound
 # end
 
-
-# put '/user/:uzer' do
+# put '/user/:user' do
     # n = User.where(:username => params[:uzer]).to_a.first
     # n.edit = params[:edit] ? 1 : 0
     # n.save
     # redirect '/'
 # end
 
-# get '/user/delete/:uzer' do
+# get '/user/delete/:user' do
     # # protected!
     # n = User.where(:username => params[:uzer]).to_a.first
     # if n.username == "Admin"
